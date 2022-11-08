535f3ac75d959a8c4a414f91a41cb3b5ea80cc3f
AkaSakana <aka_sakana@sina.com>
2022-11-08 15:17:34 +0800

    Day12: js 基础要件 完成

--- HTML/MDN_Learning/javascript/building-blocks/gallery/main.js
+++ HTML/MDN_Learning/javascript/building-blocks/gallery/main.js
@@ -1,31 +1,43 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
-/* Looping through images */
+/* 添加图片循环 */
 
-for(let i = 1; i <= 5; i++) {
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
-  thumbBar.appendChild(newImage);
-  newImage.onclick = function(e) {
-    displayedImage.src = e.target.src;
-  }
+const imgs = [
+  "pic1.jpg",
+  "pic2.jpg", 
+  "pic3.jpg", 
+  "pic4.jpg", 
+  "pic5.jpg"];
+
+for (i = 0; i < imgs.length; i++) {
+  const newImg = document.createElement("img");
+  newImg.setAttribute("src", "./images/" + imgs[i]);
+
+  // newImg.addEventListener("click", () => {
+  //   const currentImgSrc = newImg.getAttribute("src");
+  //   displayedImage.setAttribute("src", currentImgSrc);
+  // })
+
+  thumbBar.appendChild(newImg);
+
+  // another way: delegate to thumbBar
+  thumbBar.addEventListener("click", (e) => {
+    // const currentImgSrc = e.target.getAttribute("src");
+    displayedImage.setAttribute("src", e.target.src);
+  })
 }
 
-/* Wiring up the Darken/Lighten button */
-
-btn.onclick = function() {
-  const btnClass = btn.getAttribute('class');
-  if(btnClass === 'dark') {
-    btn.setAttribute('class','light');
-    btn.textContent = '变亮';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+/* 编写 变暗/变量 按钮功能 */
+btn.addEventListener("click", () => {
+  if (btn.getAttribute("class") === "dark") {
+    btn.setAttribute("class", "light");
+    overlay.style.backgroundColor = "rgba(0,0,0,.5)";
   } else {
-    btn.setAttribute('class','dark');
-    btn.textContent = '变暗';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    btn.setAttribute("class", "dark");
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
   }
-}
+})
